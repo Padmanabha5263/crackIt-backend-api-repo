4ab06a88d0c7ddd8f2641620ff3c3ea5e636060d
padmanabha <npadmanabha319@gmail.com>
2026-03-14 14:09:04 +0530

    added middleware and validation for the login api, reset password, confirm password api

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import { createUser, loginUser, ResetPasswordController, sendResetPasswordLinkEmail } from "../controllers/auth.controller";
-import { signupValidators } from "../validators/authValidators";
+import { signupValidators, logInValidators,resetPasswordValidators, resetPasswordLinkValidators } from "../validators/authValidators";
 import { validatorMiddleware } from "../middlewares/validator.middleware";
 // import { isAuth } from "../middlewares/isauth.middleware.ts";
 
 const router = Router();
 
 // auth/login
-router.post("/login", loginUser );
+router.post("/login",logInValidators, validatorMiddleware, loginUser );
 router.post("/signup", signupValidators, validatorMiddleware, createUser);
-router.post("/resetpassword", sendResetPasswordLinkEmail)
-router.post("/resetpassword/confirm", ResetPasswordController)
+router.post("/resetpassword", resetPasswordValidators, validatorMiddleware, sendResetPasswordLinkEmail)
+router.post("/resetpassword/confirm", resetPasswordLinkValidators, validatorMiddleware, ResetPasswordController)
 
 
 export default router;
--- src/validators/authValidators.ts
+++ src/validators/authValidators.ts
@@ -11,3 +11,24 @@ export const signupValidators = [
     body("dob").isDate().withMessage("Please provide a valid date of birth")
 
 ]
+
+export const logInValidators =[
+    body("email").notEmpty().withMessage("Email is required.")
+        .isEmail().withMessage("Please provide a valid email address."),
+    body("password").notEmpty().withMessage("Password is required.")
+]
+
+export const resetPasswordValidators = [
+    body("email").notEmpty().withMessage("Email is required.")
+        .isEmail().withMessage("Please provide a valid email address.")
+]
+
+export const resetPasswordLinkValidators = [
+    body("new_password").isLength({min: 6}).withMessage("Password must be at least 6 characters long."),
+    body("token").notEmpty().withMessage("Token is required."),
+    body("email").notEmpty().withMessage("Email is required.")
+        .isEmail().withMessage("Please provide a valid email address."),
+    body("confirm_password").notEmpty().withMessage("Confirm password is required.")
+        .custom((value, {req}) => value === req.body.new_password).withMessage("Passwords do not match.")
+]
+
